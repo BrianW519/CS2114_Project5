7cf082e0e0cb305c0fe179b0b98dfbae4503064e
brianwood <brianwood@localhost>
2019-11-13 16:56:49 -0500

    Created Classes

---
+++ src/prj5/FileReader.java
@@ -0,0 +1,16 @@
+package prj5;
+
+/**
+ * Description
+ *
+ * @author Brian Wood brianwood
+ * @author Dave
+ * @author David
+ * @version Nov 13, 2019
+ */
+public class FileReader {
+
+    private SongList<Song> songs;
+    
+    
+}
