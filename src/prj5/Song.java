8eb367ef52242fb01dffd2ffd6650211482fc9c9
brianwood <brianwood@localhost>
2019-11-13 17:06:06 -0500

    Song

--- src/prj5/Song.java
+++ src/prj5/Song.java
@@ -10,4 +10,30 @@ package prj5;
  */
 public class Song {
 
+    private String title;
+    private String artistName;
+    private String genre;
+    private int date;
+    private Category hobby;
+    private Category major;
+    private Category region;
+
+
+    public Song(String title, String artist, String genre, int date) {
+        this.title = title;
+        artistName = artist;
+        this.genre = genre;
+        this.date = date;
+
+    }
+    
+    public void updateData(SubEnum hobby, SubEnum major, SubEnum region, int likes) {
+        
+    }
+    
+    public Category getCategory(Category x) {
+        
+        return null;
+    }
+
 }
